fedd0f1b4090686aa987aa981bfb8fedd4caa8bf
Max Butler <max.butler@warwick.ac.uk>
2024-05-15 17:00:59 +0100

    turbo

---
+++ bo/acquisition_functions/Turbo.py
@@ -0,0 +1,115 @@
+
+import math
+import os
+from dataclasses import dataclass
+
+import torch
+
+from torch import Tensor
+from botorch.models.model import Model
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+dtype = torch.double
+tkwargs = {"device": device, "dtype": dtype}
+
+SMOKE_TEST = os.environ.get("SMOKE_TEST")
+
+@dataclass
+class ScboState:
+    dim: int
+    batch_size: int
+    length: float = 0.8
+    length_min: float = 0.5**7
+    length_max: float = 1.6
+    failure_counter: int = 0
+    failure_tolerance: int = float("nan")  # Note: Post-initialized
+    success_counter: int = 0
+    success_tolerance: int = 10  # Note: The original paper uses 3
+    best_value: float = -float("inf")
+    best_constraint_values: Tensor = torch.ones(2, **tkwargs) * torch.inf
+    restart_triggered: bool = False
+
+    def __post_init__(self):
+        self.failure_tolerance = math.ceil(max([4.0 / self.batch_size, float(self.dim) / self.batch_size]))
+
+
+def update_tr_length(state: ScboState):
+    # Update the length of the trust region according to
+    # success and failure counters
+    # (Just as in original TuRBO paper)
+    if state.success_counter == state.success_tolerance:  # Expand trust region
+        state.length = min(2.0 * state.length, state.length_max)
+        state.success_counter = 0
+    elif state.failure_counter == state.failure_tolerance:  # Shrink trust region
+        state.length /= 2.0
+        state.failure_counter = 0
+
+    if state.length < state.length_min:  # Restart when trust region becomes too small
+        state.restart_triggered = True
+
+    return state
+
+## TODO update function for decoupled evaluations
+def get_best_index_for_batch(Y: Tensor, C: Tensor):
+    """Return the index for the best point."""
+    is_feas = (C <= 0).all(dim=-1)
+    if is_feas.any():  # Choose best feasible candidate
+        score = Y.clone()
+        score[~is_feas] = -float("inf")
+        return score.argmax()
+    return C.clamp(min=0).sum(dim=-1).argmin()
+
+
+def update_state(state: ScboState, x_best_location: Tensor, model: Model):
+    """Method used to update the TuRBO state after each step of optimization.
+
+    Success and failure counters are updated according to the objective values
+    (Y_next) and constraint values (C_next) of the batch of candidate points
+    evaluated on the optimization step.
+
+    As in the original TuRBO paper, a success is counted whenver any one of the
+    new candidate points improves upon the incumbent best point. The key difference
+    for SCBO is that we only compare points by their objective values when both points
+    are valid (meet all constraints). If exactly one of the two points being compared
+    violates a constraint, the other valid point is automatically considered to be better.
+    If both points violate some constraints, we compare them inated by their constraint values.
+    The better point in this case is the one with minimum total constraint violation
+    (the minimum sum of constraint values)"""
+
+    # Pick the best point from the batch
+
+    x_best_posterior = model.posterior(x_best_location)
+    y_next = x_best_posterior.mean[0]
+    c_next = x_best_posterior.mean[1:]
+    if (c_next <= 0).all():
+        # At least one new candidate is feasible
+        improvement_threshold = state.best_value + 1e-3 * math.fabs(state.best_value)
+        if y_next > improvement_threshold or (state.best_constraint_values > 0).any():
+            state.success_counter += 1
+            state.failure_counter = 0
+            state.best_value = y_next.item()
+            state.best_constraint_values = c_next
+        else:
+            state.success_counter = 0
+            state.failure_counter += 1
+    else:  #TODO adapt this part of the code
+        # No new candidate is feasible
+        total_violation_next = c_next.clamp(min=0).sum(dim=-1)
+        total_violation_center = state.best_constraint_values.clamp(min=0).sum(dim=-1)
+        if total_violation_next < total_violation_center:
+            state.success_counter += 1
+            state.failure_counter = 0
+            state.best_value = y_next.item()
+            state.best_constraint_values = c_next
+        else:
+            state.success_counter = 0
+            state.failure_counter += 1
+
+    # Update the length of the trust region according to the success and failure counters
+    state = update_tr_length(state)
+    return state
+
+def create_trust_region(x_center: Tensor, state: ScboState):
+    tr_lb = torch.clamp(x_center - state.length / 2.0, 0.0, 1.0)
+    tr_ub = torch.clamp(x_center + state.length / 2.0, 0.0, 1.0)
+    return tr_lb, tr_ub
---
+++ bo/turbo_loop.py
@@ -0,0 +1,78 @@
+from botorch.acquisition import MCAcquisitionObjective
+from botorch.test_functions.base import BaseTestProblem
+import torch
+import time
+from torch import Tensor
+from torch._C._VariableFunctions import tensor
+from bo.acquisition_functions.Turbo import ScboState, create_trust_region
+from bo.acquisition_functions.acquisition_functions import AcquisitionFunctionType, acquisition_function_factory
+from bo.bo_loop import OptimizationLoop
+from bo.model.Model import ConstrainedDeoupledGPModelWrapper
+from bo.result_utils.result_container import Results
+
+class turbo_boloop(OptimizationLoop):
+
+    def __init__(self, black_box_func: BaseTestProblem, model: ConstrainedDeoupledGPModelWrapper, objective: MCAcquisitionObjective | None, ei_type: AcquisitionFunctionType, seed: int, budget: int, performance_type: str, bounds: Tensor, results: Results, penalty_value: Tensor | None = ..., number_initial_designs: int | None = 6, costs: Tensor | None = ...):
+        super().__init__(black_box_func, model, objective, ei_type, seed, budget, performance_type, bounds, results, penalty_value, number_initial_designs, costs)
+
+    def run(self):
+        best_observed_all_sampled = []
+        train_x, train_y = self.generate_initial_data(n=self.number_initial_designs)
+        state = ScboState()
+
+        start_time = time.time()
+        for iteration in range(self.budget):
+            best_observed_location, best_observed_value = self.best_observed(
+                best_value_computation_type=self.performance_type,
+                train_x=train_x,
+                train_y=train_y,
+                model=model,
+                bounds=self.bounds)
+            
+            tr_lb, tr_ub = create_trust_region(best_observed_location,state)
+            best_observed_all_sampled.append(best_observed_value)
+            #TODO restrict train_x/y to within trust region
+            model = self.update_model(train_x, train_y)
+            kg_values_list = torch.zeros(self.number_of_outputs, dtype=dtype)
+            new_x_list = []
+            for task_idx in range(self.number_of_outputs):
+                # print("Running Task:", task_idx)
+                acquisition_function = acquisition_function_factory(model=model,
+                                                                    type=self.acquisition_function_type,
+                                                                    objective=self.objective,
+                                                                    best_value=best_observed_value,
+                                                                    idx=task_idx,
+                                                                    number_of_outputs=self.number_of_outputs,
+                                                                    penalty_value=self.penalty_value,
+                                                                    iteration=iteration)
+
+                new_x, kgvalue = self.compute_next_sample(acquisition_function=acquisition_function, bounds=torch.stack([tr_lb,tr_ub]))  
+                kg_values_list[task_idx] = kgvalue
+                new_x_list.append(new_x)
+
+            index = torch.argmax(torch.tensor(kg_values_list)/self.costs)
+            new_y = self.evaluate_black_box_func(new_x_list[index], index)
+
+            train_x[index] = torch.cat([train_x[index], new_x_list[index]])
+            train_y[index] = torch.cat([train_y[index], new_y])
+            model = self.update_model(X=train_x, y=train_y)
+
+            print(
+                f"\nBatch {iteration:>2} finished: best value (EI) = "
+                f"({best_observed_value:>4.5f}), best location " + str(
+                    best_observed_location) + " current sample decision x: " + str(new_x_list[index]) + f" on task {index}\n",
+                end="",
+            )
+            self.save_parameters(train_x=train_x,
+                                 train_y=train_y,
+                                 best_predicted_location=best_observed_location,
+                                 best_predicted_location_value=self.evaluate_location_true_quality(
+                                     best_observed_location),
+                                 acqf_recommended_location=new_x_list[index],
+                                 acqf_recommended_location_true_value=self.evaluate_location_true_quality(new_x_list[index]),
+                                 acqf_recommended_output_index=index)
+            middle_time = time.time() - start_time
+            print(f'took {middle_time} seconds')
+        
+        end = time.time() - start_time
+        print(f'Total time: {end} seconds')
