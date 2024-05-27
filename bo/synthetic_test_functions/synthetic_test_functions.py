f118f4f8de8fd4e54be42564e3f14acd61987618
Xietao Wang Lin <Xietao.Wang-Lin@warwick.ac.uk>
2024-05-27 17:17:43 +0100

    super redundant

--- bo/synthetic_test_functions/synthetic_test_functions.py
+++ bo/synthetic_test_functions/synthetic_test_functions.py
@@ -206,6 +206,97 @@ class MysteryFunctionRedundant(ConstrainedBaseTestProblem):
             print("Error evaluate_task")
             raise
 
+class MysteryFunctionSuperRedundant(ConstrainedBaseTestProblem):
+    _bounds = [(0.0, 5.0), (0.0, 5.0)]
+
+    def __init__(self, noise_std=0.0, negate=False):
+        self.dim = 2
+        super().__init__(noise_std=noise_std, negate=negate)
+        self._bounds = torch.tensor(self._bounds, dtype=torch.float).transpose(-1, -2)
+
+    def evaluate_true(self, X: Tensor) -> Tensor:
+        X_tf = unnormalize(X, self._bounds)
+        X_1 = X_tf[..., 0]
+        X_2 = X_tf[..., 1]
+
+        t1 = 2.0 + 0.01*((X_2 - X_1.pow(2)).pow(2))
+        t2 = (1 - X_1).pow(2)
+        t3 = 2*((2-X_2).pow(2))
+        t4 = 7*torch.sin(0.5*X_1)*torch.sin(0.7*X_1*X_2)
+        return (t1 + t2 + t3 + t4)
+
+    def evaluate_slack_true(self, X: Tensor) -> Tensor:
+        pass
+    
+    def evaluate_slack1_true(self, X: Tensor) -> Tensor:
+        X_tf = unnormalize(X, self._bounds)
+        return -torch.sin(X_tf[..., 0] - X_tf[..., 1] - math.pi/8)
+    
+    def evaluate_slack2_true(self, X: Tensor) -> Tensor:
+        X_tf = unnormalize(X, self._bounds)
+        return X_tf[...,0]*0.0 + X_tf[...,0]*0.0 - 100
+    
+    def evaluate_slack3_true(self, X: Tensor) -> Tensor:
+        X_tf = unnormalize(X, self._bounds)
+        return X_tf[...,0]*0.0 + X_tf[...,0]*0.0 - 100
+    
+    def evaluate_slack4_true(self, X: Tensor) -> Tensor:
+        X_tf = unnormalize(X, self._bounds)
+        return X_tf[...,0]*0.0 + X_tf[...,0]*0.0 - 100
+    
+    def evaluate_slack5_true(self, X: Tensor) -> Tensor:
+        X_tf = unnormalize(X, self._bounds)
+        return X_tf[...,0]*0.0 + X_tf[...,0]*0.0 - 100
+    
+    def evaluate_slack6_true(self, X: Tensor) -> Tensor:
+        X_tf = unnormalize(X, self._bounds)
+        return X_tf[...,0]*0.0 + X_tf[...,0]*0.0 - 100
+    
+    def evaluate_slack7_true(self, X: Tensor) -> Tensor:
+        X_tf = unnormalize(X, self._bounds)
+        return X_tf[...,0]*0.0 + X_tf[...,0]*0.0 - 100
+    
+    def evaluate_slack8_true(self, X: Tensor) -> Tensor:
+        X_tf = unnormalize(X, self._bounds)
+        return X_tf[...,0]*0.0 + X_tf[...,0]*0.0 - 100
+    
+    def evaluate_slack9_true(self, X: Tensor) -> Tensor:
+        X_tf = unnormalize(X, self._bounds)
+        return X_tf[...,0]*0.0 + X_tf[...,0]*0.0 - 100
+
+    def evaluate_black_box(self, X: Tensor) -> Tensor:
+        y = self.evaluate_true(X).reshape(-1, 1)
+        c1 = self.evaluate_slack1_true(X).reshape(-1, 1)
+        c2 = self.evaluate_slack2_true(X).reshape(-1, 1)
+        return torch.concat([y, c1, c2], dim=1)
+
+    def evaluate_task(self, X: Tensor, task_index: int) -> Tensor:
+        assert task_index <= 9, "Maximum of 3 Outputs allowed (task_index <= 2)"
+        assert task_index >= 0, "No negative values for task_index allowed"
+        if task_index == 0:
+            return self.forward(X)
+        elif task_index == 1:
+            return self.evaluate_slack1_true(X)
+        elif task_index == 2:
+            return self.evaluate_slack2_true(X)
+        elif task_index == 3:
+            return self.evaluate_slack3_true(X)
+        elif task_index == 4:
+            return self.evaluate_slack4_true(X)
+        elif task_index == 5:
+            return self.evaluate_slack5_true(X)
+        elif task_index == 6:
+            return self.evaluate_slack6_true(X)
+        elif task_index == 7:
+            return self.evaluate_slack7_true(X)
+        elif task_index == 8:
+            return self.evaluate_slack8_true(X)
+        elif task_index == 9:
+            return self.evaluate_slack9_true(X)
+        else:
+            print("Error evaluate_task")
+            raise
+
 class ConstrainedFunc3(ConstrainedBaseTestProblem):
     _bounds = [(0.0, 1.0), (0.0, 1.0)]
 
